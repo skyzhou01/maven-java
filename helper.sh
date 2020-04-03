0958dd55fc5ed91da8948e4d202a9edc5e10ac4e
hao sky zhou <hao.s.zhou@gmail.com>
2020-04-03 17:18:29 +0100

    add gitkeep files

--- helper.sh
+++ helper.sh
@@ -5,3 +5,20 @@ cd maven-java
 mvn compile
 
 # you will see the build be successful
+
+
+# Directory Template
+# standard java project, maven will look into Directory Template
+# project root -> src -> main -> java -> resources
+
+# standard java project, maven will look into Directory Template for unit test
+# project root -> src -> test -> java and resources
+
+# standard java project, maven will look into Directory Template for web app
+# project root -> src -> main -> webapp
+
+# standard java project, maven will look into Directory Template for groovy
+# project root -> src -> main -> groovy
+
+# using the following to create 
+mkdir -p src/main/resource
